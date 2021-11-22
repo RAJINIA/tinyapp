189b9ae8f3d82929602e36807d150c3dee14ccd8
RAJINIA <rajini1608@gmail.com>
2021-11-22 14:54:01 -0600

    Updated helpers.js with comments

--- helpers.js
+++ helpers.js
@@ -1,10 +1,12 @@
+// Generate a Random ShortURL
 const generateRandomString = () => {
   let randomString = Math.random().toString(36).substr(2, 6);
   return randomString;
 };
 
+// Create URL's for the User
 const urlsForUser = (userID, urlDatabase) => {
-  console.log("URLDATABASE: ", urlDatabase);
+  // console.log("URLDATABASE: ", urlDatabase);
   const filteredUrls = {};
   for (const url in urlDatabase) {
     if (urlDatabase[url].userID === userID) {
@@ -17,6 +19,7 @@ const urlsForUser = (userID, urlDatabase) => {
   return filteredUrls;
 };
 
+// Get the User by their Email
 const getUserByEmail = (email, users) => {
   for (let id in users) {
     if (users[id].email === email) {
@@ -26,6 +29,7 @@ const getUserByEmail = (email, users) => {
   return false;
 };
 
+// Check if The User Exist
 const userAlreadyExist = (email, usersDB) => {
   for (let user in usersDB) {
     if (usersDB[user]["email"] === email) return { error: "Email Exists" };
@@ -33,6 +37,7 @@ const userAlreadyExist = (email, usersDB) => {
   return { error: null };
 };
 
+// authenticate the User Information
 const authenticateUserInfo = (email, password, usersDB) => {
   if (!email || email.trim() === "") return { error: "Invalid Email" };
   if (!password || password.trim() === "") return { error: "Invalid pasword" };
